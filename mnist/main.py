b9a480a59ff0b578890174a5438c7c94d4550845
cgianmarco <c.giammy95@gmail.com>
2017-02-25 21:32:12 +0100

    complete cnn

--- mnist/main.py
+++ mnist/main.py
@@ -5,17 +5,40 @@ from im2col import im2col_indices, col2im_indices
 filter_size = 3
 n_filters = 20
 n_filters_3 = 10
+weights_scale = 0.01
+epsilon = 0.00005
+epochs = 1000
+
+
+n_classes = 10
+
+def set_item(array, index, value):
+	array[index] = value
+	return array
+
+def get_one_encoded(Y):
+	return np.array([ set_item(np.zeros(n_classes), y, 1) for y in Y ])
+
 
 digits = load_digits()
-print digits.data.shape
+print digits.data[0]
+data = digits.data - np.mean(digits.data, axis=1, keepdims=True)
+print data[0]
+# print digits.data.shape
 
-data = np.reshape(digits.data, (1797, 1, 8, 8))
-print data.shape
+Y = digits.target
+Y = get_one_encoded(Y)
+
+data = np.reshape(data, (1797, 1, 8, 8))
+# print data.shape
 
 # import matplotlib.pyplot as plt 
 # plt.gray() 
 # plt.matshow(data) 
 # plt.show()
+
+def softmax(x):
+	return np.array([np.exp(x[i])/(np.sum(np.exp(x), axis=1)[i]) for i in range(len(x))])
 
 def conv_forward(X, W, stride=1, padding=1):
     cache = W, stride, padding
@@ -55,7 +78,7 @@ def conv_backward(dout, cache):
 
     return dX, dW
 
-def max_pool_forward(X, d,  padding=0, stride=2):
+def max_pool_forward(X, d, h_in, w_in, padding=0, stride=2):
 
 	# Let say our input X is 5x10x28x28
 	# Our pooling parameter are: size = 2x2, stride = 2, padding = 0
@@ -65,11 +88,12 @@ def max_pool_forward(X, d,  padding=0, stride=2):
 	n = len(data)
 	# d = n_filters
 
-	h_out = (len(X) - filter_size)/stride + 1
-	w_out = (len(X) - filter_size)/stride + 1
+	h_out = (w_in - size)/stride + 1
+	w_out = (h_in - size)/stride + 1
 
 	# First, reshape it to 50x1x28x28 to make im2col arranges it fully in column
-	X_reshaped = X.reshape(len(data)*d, 1, 8, 8)
+	# print (len(data)*d, 1, h_in, w_in)
+	X_reshaped = X.reshape(len(data)*d, 1, h_in, w_in)
 
 	# The result will be 4x9800
 	# Note if we apply im2col to our 5x10x28x28 input, the result won't be as nice: 40x980
@@ -77,6 +101,8 @@ def max_pool_forward(X, d,  padding=0, stride=2):
 
 	# Next, at each possible patch location, i.e. at each column, we're taking the max index
 	max_idx = np.argmax(X_col, axis=0)
+
+	cache = (X, X_col, max_idx)
 
 	# Finally, we get all the max value at each column
 	# The result will be 1x9800
@@ -88,12 +114,54 @@ def max_pool_forward(X, d,  padding=0, stride=2):
 	# Transpose to get 5x10x14x14 output
 	out = out.transpose(2, 3, 0, 1)
 
-	return out
+	return out, cache
+
+def max_pool_backward(dout, d, h_in, w_in, X, X_col, max_idx, stride=2):
+	size = 2
+	n = len(data)
+
+	# X_col and max_idx are the intermediate variables from the forward propagation step
+
+	# Suppose our output from forward propagation step is 5x10x14x14
+	# We want to upscale that back to 5x10x28x28, as in the forward step
+
+	# 4x9800, as in the forward step
+	dX_col = np.zeros_like(X_col)
+
+	# 5x10x14x14 => 14x14x5x10, then flattened to 1x9800
+	# Transpose step is necessary to get the correct arrangement
+	dout_flat = dout.transpose(2, 3, 0, 1).ravel()
+
+	# Fill the maximum index of each column with the gradient
+
+	# Essentially putting each of the 9800 grads
+	# to one of the 4 row in 9800 locations, one at each column
+	dX_col[max_idx, range(max_idx.size)] = dout_flat
+
+	# We now have the stretched matrix of 4x9800, then undo it with col2im operation
+	# dX would be 50x1x28x28
+	dX = col2im_indices(dX_col, (n * d, 1, h_in, w_in), size, size, padding=0, stride=stride)
+
+	# Reshape back to match the input dimension: 5x10x28x28
+	dX = dX.reshape(X.shape)
+
+	return dX
+
+def measure_accuracy(prediction, target):
+	accuracy = 0
+	for i in range(len(target)):
+		if np.argmax(target[i]) == np.argmax(prediction[i]):
+			accuracy += 1
+	return float(accuracy)/len(target)
+
 
 # X = im2col_indices(data, 3, 3)
 # filters_1 = np.random.randn(n_filters, filter_size*filter_size)
-filters_1 = np.random.randn(n_filters, 1, filter_size, filter_size)
-filters_3 = np.random.randn(n_filters_3, 1, filter_size, filter_size)
+filters_1 = np.random.randn(n_filters, 1, filter_size, filter_size) / np.sqrt((8*8*n_filters)/2)
+filters_3 = np.random.randn(n_filters_3, n_filters, filter_size, filter_size) / np.sqrt((4*4*n_filters_3)/2)
+fc_weights = np.random.randn(2*2*n_filters_3, n_classes) / np.sqrt((2*2*n_filters_3))
+n_samples = len(data)
+
 # print X.shape
 # print filters_1.shape
 # result = filters_1.dot(X)
@@ -101,16 +169,65 @@ filters_3 = np.random.randn(n_filters_3, 1, filter_size, filter_size)
 # result = result.transpose(3, 0, 1, 2)
 # print result.shape
 
-out1, cache1 = conv_forward(data, filters_1)
-print out1.shape
+for epoch in range(epochs):
+	out1, cache1 = conv_forward(data, filters_1)
+	# print out1.shape
 
-out2 = max_pool_forward(out1, n_filters)
-print out2.shape
+	out2, cache2 = max_pool_forward(out1, n_filters, 8, 8)
+	# print out2.shape
 
-out3, cache3 = conv_forward(out2, filters_3)
-print out3.shape
+	out3, cache3 = conv_forward(out2, filters_3)
+	# print out3.shape
 
-out4 = max_pool_forward(out3, n_filters_3)
+	out4, cache4 = max_pool_forward(out3, n_filters_3, 4, 4)
+	# print out4.shape
+
+	fc_input = out4.reshape(len(data), -1)
+	
+
+	output = fc_input.dot(fc_weights)
+	prediction = softmax(output)
+
+	loss =  np.sum(-np.sum(Y * np.log(prediction), axis=1), axis=0)/n_samples
+
+	
+	if epoch % 10 == 0:
+		accuracy = measure_accuracy(prediction, Y)
+		print "Epoch number: " + str(epoch) + "    loss: " + str(loss) + "    accuracy: " + str(accuracy)
+	else:
+		print "Epoch number: " + str(epoch) + "    loss: " + str(loss)
+
+
+
+	# accuracy = measure_accuracy(prediction, Y)
+	# print "Epoch number: " + str(epoch) + "    loss: " + str(loss) + "    accuracy: " + str(accuracy)
+
+	
+
+	# print prediction.shape
+	# print prediction[0]
+
+	# print Y.shape
+
+	output_delta = prediction - Y
+	# print output_delta.shape
+	fc_weights_delta = fc_input.T.dot(output_delta)
+
+	input_delta = output_delta.dot(fc_weights.T)
+	# print input_delta.shape
+
+	d_out4 = input_delta.reshape(len(data), n_filters_3, 2, 2)
+	d_out3 = max_pool_backward(d_out4, n_filters_3, 4, 4, cache4[0], cache4[1], cache4[2])
+	d_out2, d_filters_3 = conv_backward(d_out3, cache3)
+	d_out1 = max_pool_backward(d_out2, n_filters, 8, 8, cache2[0], cache2[1], cache2[2])
+	d_data, d_filters = conv_backward(d_out1, cache1)
+	# print d_out2.shape
+
+	fc_weights -= epsilon * fc_weights_delta 
+	filters_1 -= epsilon * d_filters
+	filters_3 -= epsilon * d_filters_3
+
+
 
 
 
