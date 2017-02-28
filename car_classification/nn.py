fix softmax backprop, start graph

car_classification/nn.py
@@ -74,11 +74,12 @@ print Y.shape
 # We use a 2 layers neural network with one 8 neurons hidden layer
 
 n_inputs = 6
-n_h1 = 20
+n_h1 = 25
 n_outputs = 4
 n_samples = len(X)
 epochs = 100000
 learning_rate = 1e-0
+learning_rate = 0.01
 reg=1e-3
 
 
@@ -103,7 +104,7 @@ class Dense:
 	def __init__(self, number_of_neurons, input_dim):
 		self.number_of_neurons = number_of_neurons
 		self.input_dim = input_dim
-		self.weights = 0.01 * np.random.randn(n_inputs, number_of_neurons)
+		self.weights = 0.01 * np.random.randn(input_dim, number_of_neurons)
 
 	def forward(self, X):
 		self.input = X
@@ -115,6 +116,9 @@ class Dense:
 		dX = dout.dot(self.weights.T)
 
 		return dX, dW
+
+	def change_weights(self, delta):
+		self.weights -= learning_rate * delta
 
 
 
@@ -131,10 +135,10 @@ class Activation:
 			self.output = np.maximum(0, X)
 
 		elif self.function == "tanh":
-			self.output = np.tanh(np.tanh(0, X))
+			self.output = np.tanh(X)
 
 		elif self.function == "softmax":
-			self.output = np.array([np.exp(x[i])/(np.sum(np.exp(x), axis=1)[i]) for i in range(len(x))])
+			self.output = np.array([np.exp(X[i])/(np.sum(np.exp(X), axis=1)[i]) for i in range(len(X))])
 
 		else:
 			self.output = np.maximum(0, X)
@@ -145,29 +149,40 @@ class Activation:
 	def backward(self, dout):
 		 
 		if self.function == "ReLU":
-			gradient = 0
+			gradient = np.empty_like(self.input)
+			gradient[self.input <= 0] = 0
+			gradient[self.input > 0] = 1
 
 		elif self.function == "tanh":
 			gradient = 1 - self.output**2
 
 		elif self.function == "softmax":
-			SM = self.output.reshape((-1, 1))
-			gradient = np.diag(self.output) - np.dot(SM, SM.T)
+			# SM = self.output.reshape((-1, 1))
+			gradient = Y * self.output - self.output * self.output
+			# gradient = Y * self.output - (np.reshape(np.sum(Y * self.output, axis=1),(-1, 1))*self.output)
+			# gradient = 1
+			# print "shape is " + str(np.reshape(np.diag(self.output), (-1, 1)).shape)
+			# print "shape2 is " + str((self.output*self.output).shape)
+			# print "shape2 is " + str((np.sum(self.output*self.output, axis=1)).shape)
+			# gradient = ((dout - np.reshape(np.sum(dout*self.output, axis=1), (-1, 1))) * self.output)
+			return gradient
 		else:
-			derivative = np.maximum(0, X)
+			gradient = np.empty_like(self.input)
+			gradient[self.input <= 0] = 0
+			gradient[self.input > 0] = 1
 
 		dX = dout * gradient
 
 		return dX
 		
 
-layer_1 = Dense(n_h1, n_inputs)
-layer_2 = Activation("tanh")
-z = layer_1.forward(X)
-print z.shape
-pred = layer_2.forward(z)
-print Y.shape
-error = 
+# layer_1 = Dense(n_h1, n_inputs)
+# layer_2 = Activation("softmax")
+# z = layer_1.forward(X)
+# print z.shape
+# pred = layer_2.forward(z)
+# print Y.shape
+
 
 
 
@@ -177,39 +192,52 @@ error =
 
 # w_l1 = 0.01 * np.random.randn(n_inputs, n_h1)
 # w_output = 0.01 * np.random.randn(n_h1, n_outputs)
+layer_1 = Dense(n_h1, n_inputs)
+act_1 = Activation("ReLU")
+
+layer_2 = Dense(n_outputs, n_h1)
+act_2 = Activation("softmax")
 
 
-# for epoch in range(epochs):
+for epoch in range(epochs):
 
-# 	h1 = np.maximum(0, X.dot(w_l1))
-# 	assert h1.shape == (n_samples, n_h1), "Error on first layer"
+	z1 = layer_1.forward(X)
+	print z1.shape
+	h1 = act_1.forward(z1)
+	print h1.shape
 
-# 	prediction = softmax(h1.dot(w_output))
-# 	assert prediction.shape == (n_samples, n_outputs), "Error on second layer"
+	z2 = layer_2.forward(h1)
+	prediction = act_2.forward(z2)
+
+	# reg_loss = 0.5 * reg * np.sum(w_output*w_output) + 0.5 * reg * np.sum(w_l1 * w_l1)
+	# loss =  - np.sum(np.sum(Y * np.log(prediction), axis=1), axis=0)/n_samples
+	loss =  np.sum(-np.sum(Y * np.log(prediction), axis=1), axis=0)/n_samples
+
+	# loss = np.sum(-np.log((Y * prediction)))/n_samples
+
+	accuracy = measure_accuracy(prediction, Y)
+
+	print "Epoch number: " + str(epoch) + "    loss: " + str(loss) + "    accuracy: " + str(accuracy)
+
+	# output_delta = (prediction - Y)
+	output_delta = -Y * np.reshape(1/np.sum((Y*prediction), axis=1), (-1, 1))
+
+	# Sigmoid backprop
+	# l1_delta = output_delta.dot(w_output.T) * h1 * (1 - h1)
+
+	# tanh backprop
+	# l1_delta = output_delta.dot(w_output.T) * (1 - h1**2)
+
+	# ReLU backprop
+	# l1_delta = output_delta.dot(w_output.T)
+	# l1_delta[ h1 <= 0 ] = 0
+
+	dout_act_2 = act_2.backward(output_delta)
+	dout_layer_2, dw_layer_2 = layer_2.backward(dout_act_2)
+
+	dout_act_1 = act_1.backward(dout_layer_2)
+	dout_layer_1, dw_layer_1 = layer_1.backward(dout_act_1)
 
 
-# 	reg_loss = 0.5 * reg * np.sum(w_output*w_output) + 0.5 * reg * np.sum(w_l1 * w_l1)
-# 	loss =  np.sum(-np.sum(Y * np.log(prediction), axis=1), axis=0)/n_samples + reg_loss
-
-# 	accuracy = measure_accuracy(prediction, Y)
-
-# 	print "Epoch number: " + str(epoch) + "    loss: " + str(loss) + "    accuracy: " + str(accuracy)
-
-# 	output_delta = (prediction - Y)
-
-# 	# Sigmoid backprop
-# 	# l1_delta = output_delta.dot(w_output.T) * h1 * (1 - h1)
-
-# 	# tanh backprop
-# 	# l1_delta = output_delta.dot(w_output.T) * (1 - h1**2)
-
-# 	# ReLU backprop
-# 	l1_delta = output_delta.dot(w_output.T)
-# 	l1_delta[ h1 <= 0 ] = 0
-
-# 	w_output_delta = h1.T.dot(output_delta) * reg
-# 	w_l1_delta = X.T.dot(l1_delta) * reg
-
-
-# 	w_output -= learning_rate * w_output_delta
-# 	w_l1 -= learning_rate * w_l1_delta 
+	layer_2.change_weights(dw_layer_2)
+	layer_1.change_weights(dw_layer_1)